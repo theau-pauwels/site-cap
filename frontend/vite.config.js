8ed4baa0b0cae43316b7d0f2616946dbb9a7637a
Theau Pauwels <83910999+theau-pauwels@users.noreply.github.com>
2025-09-02 17:25:24 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
-// frontend/vite.config.js
 import { defineConfig } from 'vite';
-
 export default defineConfig({
   server: {
-    host: true,             // écoute sur 0.0.0.0 en Docker
-    port: 3000,             // ou 4321 si vous l’utilisez
+    host: true,
+    port: 3000,
     strictPort: true,
-    allowedHosts: ['frontend', 'localhost', '127.0.0.1'],
-    hmr: { host: 'localhost' } // utile derrière proxy pour le HMR
+    allowedHosts: ['frontend','localhost','127.0.0.1'],
+    hmr: { host: 'localhost' }
   }
 });
