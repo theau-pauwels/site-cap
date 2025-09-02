bcce641b073c9d747fc5001cd62c2d44dd4550dc
Theau Pauwels <83910999+theau-pauwels@users.noreply.github.com>
2025-09-02 21:07:04 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
   server: {
     host: true,
-    port: 3000,
+    port: 3000,          // 👈 on force 3000 pour coller au nginx
     strictPort: true,
-    allowedHosts: ['frontend','localhost','127.0.0.1'],
+    allowedHosts: ['frontend', 'localhost', '127.0.0.1'],
     hmr: { host: 'localhost' }
   }
 });
