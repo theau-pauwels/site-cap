a1275eda8483c23d7e89c2b5d0a2dc9c9434dc0a
Theau Pauwels <83910999+theau-pauwels@users.noreply.github.com>
2025-09-03 14:46:51 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     host: true,
     port: 3000,          // 👈 on force 3000 pour coller au nginx
     strictPort: true,
-    allowedHosts: ['frontend', 'localhost', '127.0.0.1'],
+    allowedHosts: ['frontend', 'localhost', '127.0.0.1', 'carte-fede-test.magellan.fpms.ac.be'],
     hmr: { host: 'localhost' }
   }
 });
