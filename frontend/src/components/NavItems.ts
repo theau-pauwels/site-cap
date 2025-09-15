844a279a91335362a808ffc0f1589380c65f9e59
Theau Pauwels <theau.pauwels@gmail.com>
2025-09-15 22:41:54 +0000

    V1 fonctionnel

--- frontend/src/components/NavItems.ts
+++ frontend/src/components/NavItems.ts
@@ -7,13 +7,10 @@ export type NavItem = {
 
 export const navItems: NavItem[] = [
     { label: "Se connecter", href: "/login" },
-    // { label: "S'inscrire", href: "/register" },
+    { label: "S'inscrire", href: "/register" },
     { label: "Pins", href: "/pins" },
     { label: "Changer de mot de passe", href:"/app/password" },
-    { label: "Liste d'utilisateurs", href:"/admin/users", adminOnly: true },
-    { label: "Ajouter pins", href: "/adminpins", adminOnly: true },
-    { label : "Commandes", href: "/admin/commandes", adminOnly: true},
-    {label : "Mon panier", href: "/cart"},
-    {label : "Mes commandes", href: "/myorders"},
-    // { label : "Site Fédé", href: "https://fede.fpms.ac.be" },
+    { label : "Mon panier", href: "/cart"},
+    { label : "Admin", href: "/admin", adminOnly: true},
+    { label : "Site Fédé", href: "https://fede.fpms.ac.be" },
 ];
